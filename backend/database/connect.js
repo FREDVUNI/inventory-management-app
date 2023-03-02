1664b94b28cd212112031480ad73587c50da6f02
FREDVUNI <fredvuni809@gmail.com>
2023-03-02 12:06:41 +0300

    touch up on stuff

--- backend/database/connect.js
+++ backend/database/connect.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose")
 
 const connectDB = () =>{
     try{
-        mongoose.connect(process.env,MONGO_URI,{
+        mongoose.set('strictQuery',false);
+        mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
             useUnifiedTopology:true,
-            useCreateIndex:true,
-            useNewUrlParser:true
+            // useCreateIndex:true,
+            // useFindAndModify:false,
         })
         console.log('connected to database.')
     }
