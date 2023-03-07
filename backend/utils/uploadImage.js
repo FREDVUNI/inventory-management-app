2932187a034d8b973ea6b7bc1ce1f3d94a7ba523
FREDVUNI <fredvuni809@gmail.com>
2023-03-07 07:47:01 +0300

    upload images on cloudinary

--- backend/utils/uploadImage.js
+++ backend/utils/uploadImage.js
@@ -3,26 +3,26 @@ const router = express.Router()
 const cloudinary = require("cloudinary")
 const multer = require("multer")
 
-
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
     api_key:process.env.API_KEY,
     api_secret:process.env.API_SECRET,
 })
 
-const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-      folder: "stock",
-    },
-});
+const storage = multer.diskStorage({})
+const filter = (req,file,cb) =>{
+    if(file.mimetype==="image/jpg" || file.mimetype==="image/png"){
+        cb(null,true)
+    }else{
+        cb(null,false)
+    }
+}
+const upload = multer({storage,filter})
 
-const upload = multer({ storage: storage }); 
-
-router.post("/upload",upload.single("file"),async(req,res)=>{
+router.post("/",upload.single("file"),async(req,res)=>{
     try{
-        const result = await cloudinary.v2.uploader.upload(req.file.path)
-        res.status(200).json({message:'file has been uploaded.'})
+        const result = await cloudinary.v2.uploader.upload(req.file.path,{folder:'stock'})
+        res.status(200).json({message:'file has been uploaded.',data:result})
     }
     catch(error){
         res.status(400).json(error.message)
