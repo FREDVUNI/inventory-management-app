01b0206ca4c5ea3cc6db4616b9f130dffc2a6d6c
TAATA EARL <fredvuni809@gmail.com>
2023-05-29 18:11:47 +0300

    feat: complete register and login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Home from "./pages/Home";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <Home/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    </Router>
   );
 };
 
