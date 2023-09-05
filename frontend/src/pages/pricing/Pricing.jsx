d7d1ee342ba89da2bc6fcf9416f9f15358956c02
Fred Vuni <fredvuni809@gmail.com>
2023-09-05 08:58:20 +0300

    chore: change element from html to  jsx

--- frontend/src/pages/pricing/Pricing.jsx
+++ frontend/src/pages/pricing/Pricing.jsx
@@ -30,7 +30,7 @@ const PricingPage = () => {
                         className="mr-3 h-5 w-5 text-neutral-900 dark:text-neutral-50"
                       >
                         <path
-                          stroke-linecap="round"
+                          strokeLinecap="round"
                           strokeLinejoin="round"
                           d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                         />
@@ -49,7 +49,7 @@ const PricingPage = () => {
                         className="mr-3 h-5 w-5 text-neutral-900 dark:text-neutral-50"
                       >
                         <path
-                          stroke-linecap="round"
+                          strokeLinecap="round"
                           strokeLinejoin="round"
                           d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                         />
@@ -68,7 +68,7 @@ const PricingPage = () => {
                         className="mr-3 h-5 w-5 text-neutral-900 dark:text-neutral-50"
                       >
                         <path
-                          stroke-linecap="round"
+                          strokeLinecap="round"
                           strokeLinejoin="round"
                           d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                         />
@@ -87,7 +87,7 @@ const PricingPage = () => {
                         className="mr-3 h-5 w-5 text-neutral-900 dark:text-neutral-50"
                       >
                         <path
-                          stroke-linecap="round"
+                          strokeLinecap="round"
                           strokeLinejoin="round"
                           d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                         />
@@ -106,7 +106,7 @@ const PricingPage = () => {
                         className="mr-3 h-5 w-5 text-neutral-900 dark:text-neutral-50"
                       >
                         <path
-                          stroke-linecap="round"
+                          strokeLinecap="round"
                           strokeLinejoin="round"
                           d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                         />
@@ -125,7 +125,7 @@ const PricingPage = () => {
                         className="mr-3 h-5 w-5 text-neutral-900 dark:text-neutral-50"
                       >
                         <path
-                          stroke-linecap="round"
+                          strokeLinecap="round"
                           strokeLinejoin="round"
                           d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                         />
