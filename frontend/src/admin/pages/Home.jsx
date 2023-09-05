a0fabce8fec4afedc0079e0d2821ce70148cff50
Fred Vuni <fredvuni809@gmail.com>
2023-09-05 11:53:21 +0300

    chore: check for user info on dashboard

--- frontend/src/admin/pages/Home.jsx
+++ frontend/src/admin/pages/Home.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import MainContent from "../components/MainContent";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const userState = useSelector((state) => state.user);
+  console.log(userState);
+
   const storedDarkMode = localStorage.getItem("darkMode") === "true";
   const [isSidebarClosed, setIsSidebarClosed] = useState(false);
   const [isSearchFormVisible, setIsSearchFormVisible] = useState(false);
   const [isDarkMode, setIsDarkMode] = useState(storedDarkMode);
+  const navigate = useNavigate();
 
   const handleSidebarToggle = () => {
     setIsSidebarClosed(!isSidebarClosed);
@@ -22,6 +28,12 @@ const Home = () => {
     setIsDarkMode(newDarkMode);
     localStorage.setItem("darkMode", newDarkMode);
   };
+
+  useEffect(() => {
+    if (!userState.userInfo) {
+      navigate("/");
+    }
+  }, [navigate, userState.userInfo]);
 
   return (
     <div className={`app ${isDarkMode ? "dark" : ""} admin-home-container`}>
