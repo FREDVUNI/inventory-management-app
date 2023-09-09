72fe1b39b504e58d6a29d2ab8681c466cbd7bc97
Fred Vuni <fredvuni809@gmail.com>
2023-09-09 09:36:10 +0300

    Fix: set up cors to access protected routes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 dotenv.config({ path: ".env" });
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); 
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); 
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
