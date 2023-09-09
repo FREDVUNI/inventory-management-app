030b797545bd64a0f7bb1def037a01c51fdea54d
Fred Vuni <fredvuni809@gmail.com>
2023-09-09 10:18:36 +0300

    Fix: fix route bug

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,12 +1,18 @@
-const express = require("express")
-const { createCategory, categories, category, updateCategory, deleteCategory } = require("../controllers/categoryController")
-const router = express.Router()
-const protect = require("../middleware/authMiddleware")
+const express = require("express");
+const {
+  createCategory,
+  categories,
+  category,
+  updateCategory,
+  deleteCategory,
+} = require("../controllers/categoryController");
+const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
 
-router.post("/create",protect,createCategory)
-router.get("/categories",protect,categories)
-router.get("/:slug",protect,category)
-router.patch("/:id",protect,updateCategory)
-router.delete("/:slug",protect,deleteCategory)
+router.post("/create", protect, createCategory);
+router.get("/categories", protect, categories);
+router.get("/:slug", protect, category);
+router.patch("/:id", protect, updateCategory);
+router.delete("/:slug", protect, deleteCategory);
 
-module.exports = router
+module.exports = router;
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,49 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const upload = require('../utils/imageUpload');
-const protect = require("../middleware/authMiddleware")
-const cloudinary = require("../config/cloudinary.config")
-const Product = require("../models/Product")
+const upload = require("../utils/imageUpload");
+const { protect } = require("../middleware/authMiddleware");
+const cloudinary = require("../config/cloudinary.config");
+const Product = require("../models/Product");
 
 // Route for uploading an image
-router.post('/create',upload.single('image'),protect,async(req,res) =>{
-    try{
-        const schema = joi.object({
-            product:joi.string().required().max(30).min(2),
-            category:joi.string().required(),
-            price:joi.string().required(),
-            quantity:joi.number().required(),
-            description:joi.string().required().max(250).min(15),
-            // image: joi.string().required()
-        })
-        const { error } = schema.validate(req.body)
-        if(error) return res.status(400).json(error.details[0].message)
+router.post("/create", upload.single("image"), protect, async (req, res) => {
+  try {
+    const schema = joi.object({
+      product: joi.string().required().max(30).min(2),
+      category: joi.string().required(),
+      price: joi.string().required(),
+      quantity: joi.number().required(),
+      description: joi.string().required().max(250).min(15),
+      // image: joi.string().required()
+    });
+    const { error } = schema.validate(req.body);
+    if (error) return res.status(400).json(error.details[0].message);
 
-        const { product,category,price,quantity,description } = req.body
+    const { product, category, price, quantity, description } = req.body;
 
-        const productExists = await Product.findOne({product:req.body.product})
-        if(productExists) return res.status(400).json('Product already exists')
+    const productExists = await Product.findOne({ product: req.body.product });
+    if (productExists) return res.status(400).json("Product already exists");
 
-        const result = await cloudinary.uploader.upload(req.file.path,{folder:'stock'})
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: "stock",
+    });
 
-        let new_product = new Product({
-            product,
-            category,
-            price,
-            quantity,
-            description,
-            slug:product,
-            image:result.secure_url,
-            cloudinary_id:result.public_url
-        }) 
+    let new_product = new Product({
+      product,
+      category,
+      price,
+      quantity,
+      description,
+      slug: product,
+      image: result.secure_url,
+      cloudinary_id: result.public_url,
+    });
 
-        await new_product.save()
-        return res.status(201).json('Product has been saved.')
-    }
-    catch(error){
-        return res.status(500).json(error.message)
-    }
-
+    await new_product.save();
+    return res.status(201).json("Product has been saved.");
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
 });
 
 module.exports = router;
