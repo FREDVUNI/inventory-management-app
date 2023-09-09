6d47d62c2a548210759c73a2902de1dcfe4f3ec7
Fred Vuni <fredvuni809@gmail.com>
2023-09-09 10:22:41 +0300

    Fix: fix route bug

--- backend/middleware/errorsMiddleware.js
+++ backend/middleware/errorsMiddleware.js
@@ -1,11 +1,11 @@
-const errorHandler = (err,req,res,next) =>{
-    const statusCode = statusCode ? statusCode : 500
-    res.status(statusCode)
-    
-    res.json({
-        message: err.message,
-        stack:process.env.NODE_ENV === "development" ? err.stack : null
-    })
-}
+const errorHandler = (err, req, res, next) => {
+  const statusCode = err.status || 500;
+  res.status(statusCode);
 
-module.exports = errorHandler
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "development" ? err.stack : null,
+  });
+};
+
+module.exports = errorHandler;
