1d79f88b78cb926fb310f6ec30dabd908a0f9d43
Fred Vuni <fredvuni809@gmail.com>
2023-09-11 10:06:03 +0300

    chore: change token secret

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -29,7 +29,7 @@ const authGuard = async (req, res, next) => {
   ) {
     try {
       const token = req.headers.authorization.split(" ")[1];
-      const { id } = verify(token, process.env.JWT_SECRET);
+      const { id } = verify(token, process.env.SECRET_KEY);
       req.user = await User.findById(id).select("-password");
       next();
     } catch (error) {
