e3425faa2985d17b1781e5e66528161e875f6bb3
Fred Vuni <fredvuni809@gmail.com>
2024-09-29 10:48:34 +0300

    chore: product already exists

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,7 +19,8 @@ const addProduct = async (req, res) => {
     const { product, category, price, quantity, description, image } = req.body;
     const productExists = await Product.findOne({ product: product });
 
-    if (productExists) return res.status(400).json("Product already exists");
+    if (productExists)
+      return res.status(400).json(`Product ${product} already exists`);
     const result = await cloudinary.uploader.upload(req.body.image);
 
     const new_product = new Product({
@@ -41,6 +42,8 @@ const addProduct = async (req, res) => {
 
 const getProducts = async (req, res) => {
   try {
+    const products = await Product.find();
+    return res.status(200).json(products);
   } catch (error) {
     res.status(500).json(error.message);
   }
@@ -48,6 +51,14 @@ const getProducts = async (req, res) => {
 
 const getProduct = async (req, res) => {
   try {
+    const id = req.params.id;
+
+    const product = await Product.findById(id);
+
+    if (!product)
+      return res.status(404).json(`product with ${id} doesnot exist.`);
+
+    return res.status(200).json(product);
   } catch (error) {
     res.status(500).json(error.message);
   }
@@ -55,6 +66,32 @@ const getProduct = async (req, res) => {
 
 const updateProduct = async (req, res) => {
   try {
+    const id = req.params.id;
+
+    const schema = joi.object({
+      product: joi.string().required().max(30).min(2),
+      category: joi.string().required(),
+      price: joi.string().required(),
+      quantity: joi.number().required(),
+      description: joi.string().required().max(250).min(15),
+      image: joi.string().required(),
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if (error) return res.status(400).json(error.details[0].message);
+
+    const { product, category, price, quantity, description, image } = req.body;
+    const productCheck = await Product.findByIdAndUpdate(
+      id,
+      { product, category, price, quantity, description, image },
+      { new: true }
+    );
+
+    if (!productCheck)
+      return res.status(404).json(`product with ${id} doesnot exist.`);
+
+    return res.status(200).json("Product has been updated.");
   } catch (error) {
     res.status(500).json(error.message);
   }
@@ -62,6 +99,16 @@ const updateProduct = async (req, res) => {
 
 const deleteProduct = async (req, res) => {
   try {
+    const id = req.params.id;
+
+    const productCheck = await Product.findById(id);
+
+    if (!productCheck)
+      return res.status(404).json(`product with ${id} doesnot exist.`);
+
+    await Product.findByIdAndDelete(id);
+
+    return res.status(200).json("Product has been deleted.");
   } catch (error) {
     res.status(500).json(error.message);
   }
